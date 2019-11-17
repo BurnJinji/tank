927066b5f0afcfacff3e06e111cbd01bce69dc2b
DESKTOP-CRI8Q20\Pang <pang798763716@outlook.com>
2019-11-17 14:33:09 +0800

    添加键盘处理，根据上下左右箭头的按键状态，判定坦克的方向

--- src/com/burning8393/tank/TankFrame.java
+++ src/com/burning8393/tank/TankFrame.java
@@ -28,19 +28,54 @@ public class TankFrame extends Frame {
     public void paint(Graphics g) {
         System.out.println("paint");
         g.fillRect(x, y, 50, 50);
-        x += 10;
+        // x += 10;
+        // y += 10;
     }
 
     class MyKeyListener extends KeyAdapter {
+        boolean bL = false;
+        boolean bR = false;
+        boolean bU = false;
+        boolean bD = false;
+
         @Override
         public void keyPressed(KeyEvent e) {
-            x += 200;
-            // repaint();
+            switch (e.getKeyCode()) {
+                case KeyEvent.VK_UP:
+                    bU = true;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bD = true;
+                    break;
+                case KeyEvent.VK_LEFT:
+                    bL = true;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    bR = true;
+                    break;
+                default:
+                    break;
+            }
         }
 
         @Override
         public void keyReleased(KeyEvent e) {
-            System.out.println("key released");
+            switch (e.getKeyCode()) {
+                case KeyEvent.VK_UP:
+                    bU = false;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bD = false;
+                    break;
+                case KeyEvent.VK_LEFT:
+                    bL = false;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    bR = false;
+                    break;
+                default:
+                    break;
+            }
         }
     }
 
