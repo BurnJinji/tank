5f2f5360944267a5b2ce4e5c10120386cd492ce8
DESKTOP-CRI8Q20\Pang <pang798763716@outlook.com>
2019-11-18 22:48:44 +0800

    加入配置文件，让程序更灵活

--- src/com/burning8393/tank/Main.java
+++ src/com/burning8393/tank/Main.java
@@ -6,9 +6,14 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         TankFrame tf = new TankFrame();
 
-        for (int i = 0; i < 5; i++) {
+        int initTankCount = Integer.parseInt((String)PropertyMgr.get("initTankCount"));
+
+        for (int i = 0; i < initTankCount; i++) {
             tf.enemies.add(new Tank(50 + 80 * i, 200, Dir.DOWN, Group.BAD, tf));
         }
+
+        new Thread(()->new Audio("audio/war1.wav").loop()).start();
+
         while (true) {
             TimeUnit.MILLISECONDS.sleep(50);
             tf.repaint();
